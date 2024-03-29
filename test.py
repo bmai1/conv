fbba6b169416a808f52aeea43485b4a09ae654ea
Brian Mai <0hrs3g@gmail.com>
2024-03-29 09:21:04 -0700

    remove test

--- test.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-
-root = tk.Tk()
-root.title("Debug Window")
-root.geometry("400x300")
-root.mainloop()
-
-# chmod +x run_convertor.zsh
-# alias run_main='/path/to/run_convertor.zsh'
