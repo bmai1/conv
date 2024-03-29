212c0a855df6439e9685c7d92d3a12aa96636921
Brian Mai <0hrs3g@gmail.com>
2024-03-29 09:21:11 -0700

    give run access

--- run_convertor.zsh
+++ run_convertor.zsh
@@ -1,2 +1,6 @@
-cd /path/to/directory
+# chmod +x run_convertor.zsh
+# alias run_main='/path/to/run_convertor.zsh'
+
+cd /path/to/directory/conv
 python3 main.py
+osascript -e 'tell application "Terminal" to quit'
