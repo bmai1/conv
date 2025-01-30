2fb4089b4ed7712645af0e6c2a78bc391b630c20
Brian Mai <0hrs3g@gmail.com>
2025-01-30 12:21:13 -0800

    Add cookies to bypass YouTube bot filter

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
+import os
 import tkinter as tk
 from tkinter import messagebox
-
 import yt_dlp
-from pytube import YouTube
-
-import os
 
 
 class Convertor:
@@ -13,39 +10,28 @@ class Convertor:
 
         root.title("Convertor")
         root.geometry("600x300")
-        # root.configure(background='black')
 
         self.center_window(root)
 
         self.url_label = tk.Label(root, text="Enter YouTube URL:", font=("Helvetica Neue", 15))
         self.url_label.pack(pady=(75, 5))
 
-
         self.url_entry = tk.Entry(root, width=50)
         self.url_entry.pack(pady=10)
-
         
         self.mp3_button = tk.Button(root, text="Download as MP3", font=("Helvetica Neue", 12), command=self.download_mp3)
         self.mp3_button.pack()
-
-        self.mp4_button = tk.Button(root, text="Download as MP4", font=("Helvetica Neue", 12), command=self.download_mp4)
-        self.mp4_button.pack()
 
         self.download_status_label = tk.Label(root, text="")
         self.download_status_label.pack()
 
     def center_window(self, window):
-
         screen_width = window.winfo_screenwidth()
         screen_height = window.winfo_screenheight()
-
-        # Calculate the position of the window
         x = (screen_width - window.winfo_reqwidth()) // 2
         y = (screen_height - window.winfo_reqheight()) // 2
-
         x -= 200
         y -= 200
-        # Set the position of the window
         window.geometry('+{}+{}'.format(x, y))
 
     def download_mp3(self):
@@ -63,33 +49,16 @@ class Convertor:
                     'preferredcodec': 'mp3',
                     'preferredquality': '192',
                 }],
-                # output path
                 'outtmpl': os.path.join('output', '%(title)s.%(ext)s'),
+                'cookiefile': 'cookies.txt' # cookies.txt Firefox extension to bypass YouTube bot filter
             }
 
             with yt_dlp.YoutubeDL(ydl_opts) as ydl:
                 info = ydl.extract_info(url, download=True)
                 title = info.get('title', 'Title Unavailable')
-                
+
             self.download_status_label.config(text=f"Download complete: {title}")
 
-        except Exception as e:
-            print(e)
-
-    def download_mp4(self):
-        url = self.url_entry.get()
-        url = self.remove_playlist_slug(url)
-
-        try:
-            yt = YouTube(url)
-
-            # stream = yt.streams.filter(file_extension='mp4').first()
-            # stream.download("output_mov")
-
-            yd = yt.streams.get_highest_resolution()
-            yd.download("output_mov")
-
-            self.download_status_label.config(text=f"Download complete: {yt.title}")
         except Exception as e:
             print(e)
 
